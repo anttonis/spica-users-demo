0975535ee4fb4ab2bceab07531d58cf2e1a9c777
Anttoni Stoimenov <anttoni.stoimenov@gmail.com>
2020-01-11 20:50:08 +0000

    added employee grid with search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridListComponent } from './sandbox/grid-list/grid-list.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { UsersComponent } from './users/users.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridListComponent,
-    HeaderComponent
+    HeaderComponent,
+    UsersComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     AgGridModule.withComponents(null)
   ],
   providers: [],
---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../models/Employee';
+import { EmployeeService } from '../../services/employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  
+  private gridApi;
+  private gridColumnApi;
+  private searchValue;
+
+  employeeList: Employee[];
+
+  private columnDefs = [
+    {headerName: 'Id', field: 'Id', getQuickFilterText: () => ''},
+    {headerName: 'LastName', field: 'LastName'},
+    {headerName: 'FirstName', field: 'FirstName'},
+    {headerName: 'Address', field: 'Address', getQuickFilterText: () => ''}
+  ];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.employeeService.getEmployees().subscribe(data => {
+      this.employeeList = data;
+    })
+  }
+
+  onGridReady(params): void {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.gridApi.sizeColumnsToFit();
+  }
+
+  //https://www.ag-grid.com/javascript-grid-filter-quick/
+  quickSearch() {
+    this.gridApi.setQuickFilter(this.searchValue);
+  }
+
+}
